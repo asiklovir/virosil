1d7ae6695d32e824e5d9fa52412c372120051616
asiklovir <127260728+asiklovir@users.noreply.github.com>
2023-03-08 04:17:19 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -17,44 +17,24 @@ from YukkiMusic import app
 
 def start_pannel(_):
     buttons = [
-        [
+       
+[
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
-            ),
+                text=_["S_B_8"], callback_data="settings_back_helper"
+            )
+],
+[
             InlineKeyboardButton(
                 text=_["S_B_2"], callback_data="settings_helper"
             ),
-        ],
+       
+            InlineKeyboardButton(
+                text= "OCTO DUYURU 🐙",
+                url=f"https://t.me/octopus_game",
+            )
+        ], 
     ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                ),
-                InlineKeyboardButton(
-                    text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                ),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                    )
-                ]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                    )
-                ]
-            )
+
     return buttons
 
 
@@ -127,8 +107,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                         text=_["S_B_7"], user_id=OWNER
                     ),
                 ]
-            )
-    buttons.append(
-        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")]
-    )
+            )    
+
     return buttons
